16cb8b01809d9547a2fcf34f9616482df1dcba38
rofdo <git.hub@ewe.net>
2025-01-01 16:52:20 +0100

    added flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Cross compiling a rust program for windows";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [
+          (import rust-overlay)
+        ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            # Openssl is for example required by the `reqwest` crate
+            openssl
+            pkg-config
+            rust-bin.stable.latest.default
+          ];
+        };
+      }
+    );
+}
+
