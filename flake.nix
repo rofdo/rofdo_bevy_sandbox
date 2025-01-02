1e2323243d8355b55bd7caae19905d3b24d05529
rofdo <git.hub@ewe.net>
2025-01-02 12:45:56 +0100

    added gitignore updated flake

--- flake.nix
+++ flake.nix
@@ -8,25 +8,32 @@
   };
 
   outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [
-          (import rust-overlay)
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [ (import rust-overlay) ];
+      };
+    in
+    {
+      devShells.default = with pkgs; mkShell {
+        buildInputs = [
+          # Openssl is for example required by the `reqwest` crate
+          openssl
+          pkg-config
+          rust-bin.stable.latest.default
         ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-      in
-      {
-        devShells.default = with pkgs; mkShell {
-          buildInputs = [
-            # Openssl is for example required by the `reqwest` crate
-            openssl
-            pkg-config
-            rust-bin.stable.latest.default
-          ];
-        };
-      }
-    );
+      };
+
+      packages.x86_64-linux.default = pkgs.rustPlatform.buildRustPackage {
+        cargoLock.lockFile = ./Cargo.lock;
+        src = ./.;
+        name = "my-bevy-sandbox";
+        buildInputs = with pkgs; [
+          openssl
+          pkg-config
+          rust-bin.stable.latest.default
+        ];
+      };
+    };
 }
 
