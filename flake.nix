5a25d0d85697c47af15ab5da7083efda9fe476a0
rofdo <git.hub@ewe.net>
2025-01-03 15:08:54 +0100

    created proper flake

--- flake.nix
+++ flake.nix
@@ -1,39 +1,60 @@
 {
   description = "Building my rust project";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
-
   outputs = { self, nixpkgs, rust-overlay, ... }:
     let
       pkgs = import nixpkgs {
         system = "x86_64-linux";
         overlays = [ (import rust-overlay) ];
       };
-      buildInputs = with pkgs; [
-        openssl
+      nativeBuildInputs = with pkgs; [
         pkg-config
+        gcc
         rust-bin.stable.latest.default
       ];
+      buildInputs = with pkgs; [
+        openssl
+        xorg.libX11
+        alsa-lib
+        udev
+        libxkbcommon
+        wayland
+        vulkan-loader
+        vulkan-headers
+        vulkan-tools
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXrandr
+        mesa
+        xorg.libXinerama
+        xorg.libXext
+        xorg.libXfixes
+        xorg.libXtst
+      ];
+      libraryPath = pkgs.lib.makeLibraryPath buildInputs;
     in
     {
-      devShells.default = with pkgs; mkShell {
-        inherit buildInputs;
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        LD_LIBRARY_PATH = "${libraryPath}:$LD_LIBRARY_PATH";
+        inherit buildInputs nativeBuildInputs;
       };
-
       packages.x86_64-linux.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "my_bevy_sandbox";
+        version = "0.1.0";
         cargoLock.lockFile = ./Cargo.lock;
         src = ./.;
-        name = "my_bevy_sandbox";
-        inherit buildInputs;
+        inherit buildInputs nativeBuildInputs;
+        
+        postFixup = ''
+          patchelf --set-rpath "${libraryPath}" $out/bin/my_bevy_sandbox
+        '';
       };
-
       apps.x86_64-linux.default = {
         type = "app";
         program = "${self.packages.x86_64-linux.default}/bin/my_bevy_sandbox";
       };
     };
 }
-
