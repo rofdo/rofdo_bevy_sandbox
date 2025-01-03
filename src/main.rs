c8fd29e19a9e2ce019f7c4a33fd279474df72101
rofdo <git.hub@ewe.net>
2025-01-03 15:43:07 +0100

    states

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 use bevy::prelude::*;
 
-fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+#[derive(States, Default, Clone, Copy, PartialEq, Eq, Debug, Hash)]
+enum AppState {
+    #[default]
+    Menu,
+    Game,
 }
 
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .init_state::<AppState>()
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+}
